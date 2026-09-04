dad3d3557c5b9d2e4137d23b5871c2240e4cb89c
Nyke <1626972119@QQ.COM>
2026-09-04 15:56:35 +0800

    fix: stretch profile banner container

--- src/components/ProfileHeader.tsx
+++ src/components/ProfileHeader.tsx
@@ -11,7 +11,7 @@ export function ProfileHeader({ player, isOwner = false }: ProfileHeaderProps) {
 
   return (
     <header className="profile-reveal overflow-hidden rounded-lg border border-[var(--profile-border)] bg-[var(--profile-surface)]">
-      <div className="relative aspect-[4/1] min-h-28 max-h-56 overflow-hidden bg-zinc-100 sm:aspect-[5/1]">
+      <div className="relative aspect-[4/1] min-h-28 max-h-56 w-full overflow-hidden bg-zinc-100 sm:aspect-[5/1]">
         {player.bannerUrl ? (
           // eslint-disable-next-line @next/next/no-img-element
           <img src={player.bannerUrl} alt={`${player.displayName} profile banner`} className="absolute inset-0 block h-full w-full max-w-none object-cover" />
